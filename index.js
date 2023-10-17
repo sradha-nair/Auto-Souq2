bfe76a16bacbf63b21913e121a820d3ebe60a2bd
sradha-nair <89587855+sradha-nair@users.noreply.github.com>
2023-10-17 19:19:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,6 +110,32 @@ const Index = () => {
         </div>
       </form>
     </div>
+    <footer>
+    <div class="footer">
+    <div class="row">
+    <a href="#"><i class="fa fa-facebook"></i></a>
+    <a href="#"><i class="fa fa-instagram"></i></a>
+    <a href="#"><i class="fa fa-youtube"></i></a>
+    <a href="#"><i class="fa fa-twitter"></i></a>
+    </div>
+    
+    <div class="row">
+    <ul>
+    <li><a href="#">Contact us</a></li>
+    <li><a href="#">Our Services</a></li>
+    <li><a href="#">Privacy Policy</a></li>
+    <li><a href="#">Terms & Conditions</a></li>
+    </ul>
+    </div>
+    
+    <div class="row">
+    AutoSouq Copyright © 2023 AutoSouq - All rights reserved 
+    </div>
+    </div>
+    </footer>
+
+
+
   );
 };
 
